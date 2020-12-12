8107001aae3f4c20f8108e7d9e5ce7689415a3b4
Hasan Berk Mahmutoğlu <hasan-lincoln-gs@hotmail.com>
2020-12-12 14:57:55 +0300

    Last modified

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 </body>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
-<script src="assets/js/a.js"></script>
+<script src="assets/js/magicsuggest-min.js"></script>
 
 <script>
 	$(function() {
---
+++ result.php
@@ -0,0 +1,55 @@
+<?php
+if(isset($_GET["s"]) && strlen($_GET["s"]) > 0)
+{
+	?>
+	<!DOCTYPE HTML>
+	<html>
+
+	<head>
+		<link rel="stylesheet" href="assets/css/magicsuggest-min.css">
+		<link rel="stylesheet" href="assets/css/main.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+	</head>
+
+	<body>
+		<h1 class="text-center mb-5 mt-5"><u class="text-primary"><?= $_GET["s"]; ?></u> ile ilgili sonuçlar</h1>
+		<div class="container">
+
+			<div class="card-deck">
+				<?php
+				include_once("api/db.php");
+
+				$stmt = $pdo->prepare("SELECT * FROM products WHERE name LIKE ?");
+				$stmt->execute([ "%". ($_GET["s"]=='all'?'':$_GET["s"]) ."%" ]);
+
+				foreach ($stmt as $search)
+				{
+					?>
+					<div class="card">
+						<img class="card-img-top" src="assets/uploads/<?= $search['image']; ?>" alt="Card image cap">
+						<div class="card-body">
+							<h5 class="card-title"><?= $search['name']; ?></h5>
+							<p class="card-text">Stoktaki Ürün: <?= $search['quantity']; ?></p>
+						</div>
+						<div class="card-footer">
+							<small class="text-muted">Eklenme Tarihi: <?= date("d.m.Y", strtotime($search['created_date'])); ?></small>
+						</div>
+					</div>
+					<?php
+				}
+				?>
+			</div>
+		</div>
+	</body>
+
+	</html>
+
+
+	<?php
+}
+else
+{
+	echo "Bir Sorun Oluştu.";
+}
+?>
